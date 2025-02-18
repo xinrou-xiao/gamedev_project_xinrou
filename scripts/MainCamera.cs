53b77f34b0ceeedfe11a347e541dc2113e808f31
Braxton Cuneo <braxton.s.cuneo@gmail.com>
2025-02-18 12:02:30 -0800

    Commit for machine switch

--- scripts/MainCamera.cs
+++ scripts/MainCamera.cs
@@ -4,7 +4,20 @@ using System;
 public partial class MainCamera : Camera3D
 {
 	[Export]
-	public Thing target { get; set; } = null;
+	public Node3D target { get; set; } = null;
+	
+	[Export]
+	public Node3D anchor { get; set; } = null;
+	
+	[Export]
+	public float tether_min { get; set; } = 0;
+	
+	[Export]
+	public float tether_max { get; set; } = 0;
+	
+	[Export]
+	public float tether_lerp { get; set; } = 0;
+	
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -14,6 +27,25 @@ public partial class MainCamera : Camera3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Transform = Transform.LookingAt(target.Transform.Origin);
+		if (target != null) {
+			Transform = Transform.LookingAt(target.Transform.Origin);
+		}
+		if (anchor != null) {
+			float current_dist = Transform.Origin.DistanceTo(anchor.Transform.Origin);
+			float coeff = 0;
+			if (current_dist > tether_max) {
+				coeff = 1; 
+			}
+			else if (current_dist < tether_min) {
+				coeff = -1;
+			}
+			float t = 1 - 1/(float)Math.Pow(Math.E,(float)delta*tether_lerp);
+			Vector3 destination = Transform.Origin.Lerp(
+				anchor.Transform.Origin,
+				t*coeff
+			);
+			Vector3 offset = destination - Transform.Origin;
+			Transform = Transform.Translated(offset);
+		}
 	}
 }
--- scripts/Pal.cs
+++ scripts/Pal.cs
@@ -3,7 +3,12 @@ using System;
 
 public partial class Pal : Thing
 {
-
+	[Export]
+	public Node3D Camera { get; set; } = null;
+	
+	[Export]
+	public RigidBody3D RigidBody { get; set; } = null;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -12,6 +17,23 @@ public partial class Pal : Thing
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Position = Position with { X = Position.X + (float) (delta * 0.5d) };
+		Vector3 to_cam  = Camera.Transform.Origin - Transform.Origin;
+		Vector3 away    = to_cam.Normalized();
+		Vector3 up      = new Vector3(0,1,0);
+		Vector3 right   = away.Cross(up);
+		Vector3 forward = up.Cross(right);
+		float dist      = to_cam.Magnetude();
+		Vector3 force   = new Vector3(0,0,0);
+		if (Input.IsActionPressed("move_right")) {
+			
+		} else if (Input.IsActionPressed("move_right")) {
+			
+		} else if (Input.IsActionPressed("move_right")) {
+			
+		} else if (Input.IsActionPressed("move_right")) {
+			
+		}
+		RigidBody.ApplyCentralImpulse(force*new Vec3(delta,delta,delta));
 	}
+	
 }
