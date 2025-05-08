c896efed97c4e193e4064f65e9eb63bd06c77a63
xinrou <xxiao@seattleu.edu>
2025-05-07 18:30:56 -0700

    fixed money count not updated  bug

--- scirpts/hud.gd
+++ scirpts/hud.gd
@@ -7,15 +7,16 @@ var money_count = 0
 
 func _on_money_collected() -> void:
 	money_count = money_count + 10
+	update_money_count()
 	
 func update_money_count():
-	money_label.text = money_count
+	money_label.text = str(money_count)
 	
 func _process(delta: float) -> void:
-		if InteractableFacade.current_target == null:
-			hide_hint()
-		else:
-			show_hint()
+	if InteractableFacade.current_target == null:
+		hide_hint()
+	else:
+		show_hint()
 	
 func show_hint():
 	hint.text = "[E]"
